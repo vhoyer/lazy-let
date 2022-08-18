6ec2885c79949a41023110d54e96c9c80f281f26
Vinícius Hoyer <contact@vhoyer.dev>
2022-08-18 15:10:09 -0300

    :sparkles: add lazylet functionality

---
+++ index.test.ts
@@ -0,0 +1,55 @@
+import { lazylet } from './index';
+
+test('lazylet defines variables', () => {
+  const $ = lazylet({
+    var1: () => 'abc',
+  });
+
+  expect($.var1).toEqual('abc');
+});
+
+test('lazylet defines lazy variables', () => {
+  const var1 = jest.fn(() => 'abcd');
+
+  const $ = lazylet({ var1 });
+
+  expect(var1).not.toHaveBeenCalled();
+  $.var1;
+  expect(var1).toHaveBeenCalled();
+})
+
+test('lazylet does not call factory twice', () => {
+  const var1 = jest.fn(() => 'abcd');
+
+  const $ = lazylet({ var1 });
+
+  expect(var1).not.toHaveBeenCalled();
+  $.var1;
+  expect(var1).toHaveBeenCalledTimes(1);
+  $.var1;
+  expect(var1).toHaveBeenCalledTimes(1);
+})
+
+test('lazylet can use keys inside keys', () => {
+  const $ = lazylet({
+    var1: () => 'abc',
+    var2: () => $.var1 + 'def',
+  });
+
+  expect($.var2).toEqual('abcdef');
+})
+
+test('lazylet can be redefined with overrides after instantiated', () => {
+  let $ = lazylet({
+    var1: () => 'abc',
+    var2: () => $.var1 + 'def',
+  });
+
+  expect($.var2).toEqual('abcdef');
+
+  $ = $({
+    var1: () => 'cebola_',
+  });
+
+  expect($.var2).toEqual('cebola_def');
+})
--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+type DefDict = { [name: string]: () => any };
+
+interface AutoReturning<V> {
+  (arg0: V): AutoReturning<V>;
+};
+
+export function lazylet<
+  T extends DefDict,
+  U extends { [key in keyof T]: any },
+>(values: T): U & AutoReturning<T> {
+  const createStore: AutoReturning<T> = (overrides) => {
+    return lazylet({
+      ...values,
+      ...overrides,
+    });
+  };
+
+  Object.entries(values).map(([key, factory]) => {
+    Object.defineProperty(createStore, key, {
+      enumerable: true,
+      configurable: true,
+      get() {
+        const value = factory()
+        Object.defineProperty(createStore, key, { get: () => value });
+        return value;
+      },
+    })
+  });
+
+  return createStore as (U & AutoReturning<T>);
+};
