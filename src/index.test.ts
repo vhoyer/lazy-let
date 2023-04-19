f0ac13fb0176874999d64ba3cff1968f7c5da2f4
Vinícius Hoyer <contact@vhoyer.dev>
2023-04-19 09:51:11 -0300

    :test_tube: Modify tests to reflect desired API change

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,71 +1,60 @@
 import { lazylet } from './index';
 
 test('lazylet defines variables', () => {
-  const $ = lazylet({
-    var1: () => 'abc',
-  });
+  const reset = (fn: Function) => fn();
+  const $0 = lazylet(reset);
+  $0.$let('var1', () => 'abc');
 
-  expect($.var1).toEqual('abc');
+  expect($0.var1).toEqual('abc');
 });
 
 test('lazylet defines lazy variables', () => {
+  const reset = (fn: Function) => fn();
   const var1 = jest.fn(() => 'abcd');
 
-  const $ = lazylet({ var1 });
+  const $0 = lazylet(reset);
+  $0.$let('var1', var1);
 
   expect(var1).not.toHaveBeenCalled();
-  $.var1;
+  $0.var1;
   expect(var1).toHaveBeenCalled();
-})
+});
 
 test('lazylet does not call factory twice', () => {
+  const reset = (fn: Function) => fn();
   const var1 = jest.fn(() => 'abcd');
 
-  const $ = lazylet({ var1 });
+  const $0 = lazylet(reset);
+  $0.$let('var1', var1);
 
   expect(var1).not.toHaveBeenCalled();
-  $.var1;
+  $0.var1;
   expect(var1).toHaveBeenCalledTimes(1);
-  $.var1;
+  $0.var1;
   expect(var1).toHaveBeenCalledTimes(1);
-})
+});
 
 test('lazylet can use keys inside keys', () => {
-  const $ = lazylet({
-    var1: () => 'abc',
-    var2: () => $.var1 + 'def',
-  });
+  const reset = (fn: Function) => fn();
+  const $0 = lazylet(reset);
+  $0.$let('var1', () => 'abc');
+  $0.$let('var2', () => $0.var1 + 'def');
 
-  expect($.var2).toEqual('abcdef');
-})
+  expect($0.var2).toEqual('abcdef');
+});
 
 test('lazylet can receive the store on the factory', () => {
-  let $ = lazylet({
-    var1: () => 'abc',
-    var2: ($) => $.var1 + 'def',
-  });
+  const reset = (fn: Function) => fn();
+  const $0 = lazylet(reset);
+  $0.$let('var1', () => 'abc');
+  $0.$let('var2', () => $0.var1 + 'def');
 
-  expect($.var2).toEqual('abcdef');
+  expect($0.var2).toEqual('abcdef');
 
-  $ = $({
-    var1: () => 'cebola_',
-  });
+  const $1 = lazylet(reset, $0);
+  $1.$let('var1', () => 'cebola_');
 
-  expect($.var2).toEqual('cebola_def');
-  expect($.var2.toUpperCase()).toEqual('CEBOLA_DEF');
-})
-
-test('lazylet can be redefined with overrides after instantiated', () => {
-  let $ = lazylet({
-    var1: () => 'abc',
-    var2: () => $.var1 + 'def',
-  });
-
-  expect($.var2).toEqual('abcdef');
-
-  $ = $({
-    var1: () => 'cebola_',
-  });
-
-  expect($.var2).toEqual('cebola_def');
-})
+  expect($1.var2).toEqual('cebola_def');
+  // this tests the var2 typing
+  expect($1.var2.toUpperCase()).toEqual('CEBOLA_DEF');
+});
